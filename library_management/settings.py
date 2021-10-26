fce13200cd890939cceed612d0e337ded2c5baec
GadeSagar <45481547+GadeSagar@users.noreply.github.com>
2021-10-26 19:35:54 +0530

    some issue handled

--- library_management/settings.py
+++ library_management/settings.py
@@ -77,8 +77,12 @@ AUTH_USER_MODEL = 'accounts.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'DB_NAME',
+        'USER': 'DB_USER',
+        'PASSWORD': 'DB_PASSWORD',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
